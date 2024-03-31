2dfd0b86888547c2ee57eb95fed7ab538d3ce84e
Eliphalet Read <eliphaletread@outlook.com>
2024-03-31 12:53:46 -0300

    Reworked this to start the slide puzzle

--- src/views/ChallengePage.jsx
+++ src/views/ChallengePage.jsx
@@ -1,36 +1,27 @@
-import { Stage } from "@pixi/react";
+import { Stage, useApp, AppProvider } from "@pixi/react";
 import { useState } from "react";
 import * as ReactBootStrap from "react-bootstrap";
 import * as PIXI from "pixi.js";
-import backGroundImage from "../assets/BridgeRunner/Map.png";
-import Player from "../components/ChallengePage/BridgeSwimmer/Player.jsx";
-import Background from "../components/ChallengePage/BridgeSwimmer/Background.jsx";
+import Blocks from "../components/ChallengePage/SlidePuzzle/Blocks";
 
 const ChallengePage = () => {
-  const [view, setView] = useState({ width: 350, height: 700 });
-  const [backgroundY, setBackgroundY] = useState(-1400 + view.height);
+  const [view, setView] = useState({ width: 3072, height: 3072 });
+  const app = new PIXI.Application();
 
   return (
     <ReactBootStrap.Container fluid className="d-flex justify-content-center">
-      <Stage
-        width={view.width}
-        height={view.height}
-        options={{
-          backgroundColor: 0x012b30,
-          antialias: true,
-        }}
-      >
-        <Background
-          image={backGroundImage}
-          backgroundY={backgroundY}
-          setBackgroundY={setBackgroundY}
-        />
-        <Player
-          x={view.width / 2}
-          y={view.height * 0.8}
-          backgroundPos={backgroundY}
-        />
-      </Stage>
+      <AppProvider value={app}>
+        <Stage
+          width={view.width}
+          height={view.height}
+          options={{
+            backgroundColor: 0x012b30,
+            antialias: true,
+          }}
+        >
+          <Blocks />
+        </Stage>
+      </AppProvider>
     </ReactBootStrap.Container>
   );
 };
