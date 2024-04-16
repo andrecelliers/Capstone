9e9e7e1427dde05c1016c4bb487ba02c5d49d69a
andrecelliers <andrecelliers3@gmail.com>
2024-04-16 16:39:17 -0300

    Validation and formatting

--- src/views/RewardPage.jsx
+++ src/views/RewardPage.jsx
@@ -17,6 +17,31 @@ function RewardPage() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [validated, set_Validated] = useState(false);
+  const [form_Data, set_Form_Data] = useState({
+    name: "",
+    email: "",
+    phoneNo: "",
+  });
+
+  const submitFn = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    set_Validated(true);
+  };
+  const chngFn = (event) => {
+    const {name, value } = event.target;
+    set_Form_Data({
+      ...form_Data,
+      [name]: value,
+    });
+  };
+    
+  
   // Confetti Hook
 
   return (
@@ -34,8 +59,7 @@ function RewardPage() {
           <Button
             variant="primary"
             onClick={handleShow}
-            className="customButton"
-          >
+            className="customButton">
             Enter Prize
           </Button>
           <CustomButton text="Return" link={`/welcome/${key}`} />
@@ -44,7 +68,6 @@ function RewardPage() {
       <Row className="py-3 px-1">
         <ContentConatiner Content="In order to qualify for your prize, enter your details with the 'Enter Prize' button. Ballots will count only once per person per challenge." />
       </Row>
-
       <Modal
         show={show}
         onHide={handleClose}
@@ -55,26 +78,66 @@ function RewardPage() {
           <Modal.Title>Enter Contact Information</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={submitFn}>
             <Form.Group className="mb-3" controlId="userForm.ControlNameInput">
               <Form.Label>Full Name</Form.Label>
               <Form.Control
-                type="textarea"
+                type="text"
+                name="user"
                 placeholder="Enter name here"
                 autoFocus
+                value={form_Data.user}
+                onChange={chngFn}
+                pattern="^[a-zA-Z]+$"
+                required
+                isInvalid={
+                  validated &&
+                  !/^[a-zA-Z0-9]+$/.test(form_Data.user)
+                }
               />
+            <Form.Control.Feedback type="invalid">
+                Please enter a valid username (alphanumeric
+                characters only).
+            </Form.Control.Feedback>              
             </Form.Group>
             <Form.Group className="mb-3" controlId="userForm.ControlEmailInput">
               <Form.Label>Email Address</Form.Label>
-              <Form.Control type="email" placeholder="example@email.com" />
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="example@email.com"
+                value={form_Data.email}
+                onChange={chngFn}
+                required
+                isInvalid={
+                  validated &&
+                  !/^\S+@\S+\.\S+$/.test(form_Data.email)                  
+                }
+             />
+              <Form.Control.Feedback type="invalid">
+                  Please enter a valid email address.
+              </Form.Control.Feedback>        
             </Form.Group>
             <Form.Group className="mb-3" controlId="userForm.ControlPhoneInput">
               <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="textarea" placeholder="(555) 555-5555" />
+              <Form.Control 
+                type="number"
+                name="phoneNo"          
+                placeholder="(555) 555-5555"
+                value={form_Data.phoneNo}
+                onChange={chngFn}
+                pattern="^\d{10}$"
+                required
+                isInvalid={
+                  validated &&
+                  !/^\d{10}$/.test(form_Data.phoneNo)
+                }
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid 10-digit phone number.
+              </Form.Control.Feedback>
             </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
+            <Modal.Footer>
           <Button
             variant="secondary"
             onClick={handleClose}
@@ -82,10 +145,15 @@ function RewardPage() {
           >
             Cancel
           </Button>
-          <Button variant="primary" className="customButton">
+          <Button type="submit"
+            variant="primary" 
+            className="customButton"
+          >
             Submit
           </Button>
         </Modal.Footer>
+        </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
